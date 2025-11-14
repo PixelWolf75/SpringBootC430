e44736f91971d303141a5d917e1eaba54112b842
Eric Leon <ericleon727@gmail.com>
2025-11-14 12:14:18 +0000

    created the basic  interfaces

---
+++ TaskMaster/src/main/java/com/mthree/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.mthree.repository;
+
+import com.mthree.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
