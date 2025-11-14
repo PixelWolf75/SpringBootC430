49b158c1e5c29cf8016abfb3596d65dc8be00320
Eric Leon <ericleon727@gmail.com>
2025-11-14 11:16:46 +0000

    Added the category calss

--- TaskMaster/src/main/com/mthree/entity/Category.java
+++ TaskMaster/src/main/com/mthree/entity/Category.java
@@ -1,4 +1,15 @@
 package com.mthree.entity;
 
-public enum Category {
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Task> tasks = new ArrayList<>();
 }
+
