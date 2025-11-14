0fa3c90c5cc7e7a5b8be3271b56c14de06730583
AmyBrownMthree <amy.browne@mthree.com>
2025-11-14 17:45:40 +0000

    working db

--- TaskMaster/src/main/resources/data.sql
+++ TaskMaster/src/main/resources/data.sql
@@ -9,7 +9,7 @@ INSERT INTO task (title, description, dueDate, priority, completed, category_id)
 VALUES
 ('Finish Spring project', 'Complete the test layer', '2025-11-20', 'HIGH', false, 1),
 ('Buy christmas presents', 'bike, computer, socks', '2025-12-25', 'LOW', false, 3),
-('Clean room', 'Organize desk and wardrobe', '2025-11-21', 'MEDIUM', false, 2);
+('Clean room', 'Organize desk and wardrobe', '2025-11-21', 'MEDIUM', false, 2), --changed semi-colon here to comma
 ('Doctor appointment', 'Annual check-up at local clinic', '2025-11-22', 'MEDIUM', false, 4),
 ('Weekly meal prep', 'Cook meals for the week', '2025-11-17', 'MEDIUM', false, 4),
 ('Call family members', 'Catch up with parents and siblings over the phone', '2025-11-24', 'LOW', false, 2);
