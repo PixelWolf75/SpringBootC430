59283d9c2b0cb1fa9a67f3834b90c98a6d2b441c
willt03 <williamtoland1@gmail.com>
2025-11-14 14:12:16 +0000

    full database application

--- TaskMaster/src/main/com/mthree/entity/Category.java
+++ TaskMaster/src/main/com/mthree/entity/Category.java
@@ -15,5 +15,21 @@ public class Category {
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     private List<Task> tasks = new ArrayList<>();
+
+    public Category() {}
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public List<Task> getTasks() { return tasks; }
+    public void setTasks(List<Task> tasks) { this.tasks = tasks; }
+
 }
 
