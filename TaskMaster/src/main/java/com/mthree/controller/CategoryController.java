ef63ae129fb8f22b88ebd4510716730fc4d9eac3
Eric Leon <ericleon727@gmail.com>
2025-11-14 13:26:37 +0000

    Created the basic category controller

--- TaskMaster/src/main/java/com/mthree/controller/CategoryController.java
+++ TaskMaster/src/main/java/com/mthree/controller/CategoryController.java
@@ -3,8 +3,13 @@ package com.mthree.controller;
 import com.mthree.entity.Category;
 import com.mthree.repository.CategoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class CategoryController {
@@ -13,27 +18,32 @@ public class CategoryController {
     CategoryRepo categoryRepo;
 
     @PostMapping("/categories")
-    public String addCategory(Category category){
-        return "redirect:/";
+    public ResponseEntity<Void> addCategory(@RequestBody  Category category){
+        categoryRepo.save(category);
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
 
     @GetMapping("/categories")
-    public String getCategories(){
-        return "redirect:/";
+    public ResponseEntity<List<Category>> getCategories(){
+        List<Category> categories = categoryRepo.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(categories);
     }
 
     @GetMapping("/categories/{id}")
-    public String getCategoryById(@PathVariable("id") Long id){
-        return "redirect:/";
+    public ResponseEntity<Category> getCategoryById(@PathVariable("id") Long id){
+        Category category = categoryRepo.findById(id).orElse(null);
+        return new ResponseEntity<Category>(category, HttpStatus.OK);
     }
 
     @PutMapping("/categories/{id}")
-    public String updateCategory(@PathVariable("id") Long id){
-        return "redirect:/";
+    public ResponseEntity<Category> updateCategory(@PathVariable("id") Long id, @RequestBody Category category){
+        categoryRepo.save(category); //Will creeate new row if the id field isn't filled
+        return new ResponseEntity<Category>(category, HttpStatus.OK);
     }
 
     @DeleteMapping("/category/{id}")
-    public String deleteCategory(@PathVariable("id") Long id){
-        return "redirect:/";
+    public ResponseEntity<Void> deleteCategory(@PathVariable("id") Long id){
+        categoryRepo.deleteById(id);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 }
